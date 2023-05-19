e543c1a3e04bdda58b8ce2e90405112ce93ac105
mkonovalov <mkonovalov@griddynamics.com>
2023-05-19 23:07:47 +0300

    add spinner

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 import './style.css'
-import appstoreBtn from '/appstore.png'
+import appstoreBtn from '/giphy.gif'
 import { createAddsData } from './addsData.js'
+import {Spinner} from 'spin.js';
+
+const opts = {
+  lines: 13, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 0.7, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000000', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: 'spinner', // The CSS class to assign to the spinner
+  position: 'absolute', // Element positioning
+};
 
 document.querySelector('#app').innerHTML = `
   <div class="center"">
     <a class="link">
-      <img src="${appstoreBtn}" />
+      <div id="spinner"></div>
     </a>
   </div>
 `
+
+const target = document.getElementById('spinner');
+new Spinner(opts).spin(target);
 
 createAddsData()
